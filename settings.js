4b3061456a48a84ae02e263e26088ee2e50b6ca1
bhattijani545 <shahzaibahmmad9@gmail.com>
2024-09-09 22:21:30 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2347063956321"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "923136540169"
+global.ownername = "BHATTI JANi WB"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
